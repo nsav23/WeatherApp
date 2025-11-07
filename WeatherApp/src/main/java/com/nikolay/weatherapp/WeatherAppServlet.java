140836ef69bbb43e4df4c0d7bbbd799a692d0f84
Nikolay Savov <nsavov.23@gmail.com>
2025-11-07 12:02:18 +0200

    Code update
    Added and implemented methods for displaying temperature and humidity levels in words

--- WeatherApp/src/main/java/com/nikolay/weatherapp/WeatherAppServlet.java
+++ WeatherApp/src/main/java/com/nikolay/weatherapp/WeatherAppServlet.java
@@ -29,6 +29,7 @@ public class WeatherAppServlet extends HttpServlet{
 
         try (Scanner scanner = new Scanner(new FileReader(file))) {
             while (scanner.hasNextLine()) {
+
                 String input = scanner.nextLine().trim();
 
                 if (input.isEmpty() || input.equals("END")) continue;
@@ -45,21 +46,56 @@ public class WeatherAppServlet extends HttpServlet{
                 String numeric = rawValue.replaceAll("[^0-9.\\-]", "");
 
                 switch (label) {
+
                     case "Humidity" -> valueH = numeric;
                     case "Temperature" -> valueT = numeric;
                     case "Pressure" -> valueP = numeric;
                     case "Altitude" -> valueAlt = numeric;
+
                 }
+
             }
+
         } catch (Exception e) {
+
             e.printStackTrace();
+
         }
 
         String valueCOR;
+
         try {
+
             valueCOR = calculateCOR(valueH, valueP, valueT);
+
         } catch (Exception e) {
+
             valueCOR = "?";
+
+        }
+
+        String temperatureText;
+
+        try {
+
+            temperatureText = defineTemperatureText(valueT);
+
+        } catch (Exception e) {
+
+            temperatureText = "?";
+
+        }
+
+        String humidityText;
+
+        try {
+
+            humidityText = defineHumidityText(valueH);
+
+        } catch (Exception e) {
+
+            humidityText = "?";
+
         }
 
         String json = "{"
@@ -67,10 +103,13 @@ public class WeatherAppServlet extends HttpServlet{
                 + "\"pressure\": {\"label\": \"Pressure\", \"value\": \"" + valueP + " hPa\"}, "
                 + "\"humidity\": {\"label\": \"Humidity\", \"value\": \"" + valueH + " %\"}, "
                 + "\"temperature\": {\"label\": \"Temperature\", \"value\": \"" + valueT + " °C\"}, "
-                + "\"cor\": {\"label\": \"Chance of Rain\", \"value\": \"" + valueCOR + " %\"}"
+                + "\"cor\": {\"label\": \"Chance of Rain\", \"value\": \"" + valueCOR + " %\"}, "
+                + "\"humtext\": {\"value\": \"" + humidityText + "\"}, "
+                + "\"temptext\": {\"value\": \"" + temperatureText + "\"}"
                 + "}";
 
         res.getWriter().write(json);
+
     }
 
     //Calculate COR(chance of rain) method
@@ -92,6 +131,66 @@ public class WeatherAppServlet extends HttpServlet{
         }
 
          return String.format("%.1f", Math.max(0, Math.min(100, baseCOR)));
+
+    }
+
+    //Display temperature levels in words method
+    private String defineTemperatureText(String valueT) {
+
+        double temperature = Double.parseDouble(valueT);
+        String tempWord = "";
+
+        if (temperature <= -20){
+
+            tempWord = "Extreme cold!";
+
+        } else if (temperature > -20 && temperature <= -5) {
+
+            tempWord = "Very cold";
+
+        } else if (temperature > -5 && temperature <= 10) {
+
+            tempWord = "Cold";
+
+        } else if (temperature > 10 && temperature <= 20) {
+
+            tempWord = "Warm";
+
+        } else if (temperature > 20 && temperature <= 35) {
+
+            tempWord = "Hot";
+
+        } else if (temperature > 35) {
+
+            tempWord = "Extreme heat!";
+
+        }
+
+        return tempWord;
+
+    }
+
+    //Display humidity levels in words method
+    private String defineHumidityText(String valueH) {
+
+        double humidity = Double.parseDouble(valueH);
+        String humWord = "";
+
+        if (humidity > 0 && humidity <= 30) {
+            
+            humWord = "Dry";
+            
+        } else if (humidity > 30 && humidity <= 60) {
+
+            humWord = "Normal";
+            
+        } else if (humidity > 60) {
+
+            humWord = "Humid";
+
+        }
+
+        return humWord;
 
     }
 
